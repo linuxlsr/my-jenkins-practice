f80a573bf834f9a9e6221824eb31264e96a8915e
Charles Harvey <soops@ucla.edu>
2019-01-02 11:08:02 -0800

    testing import of jenkins parameters

--- myPythonScript.py
+++ myPythonScript.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
-
-stringParameter = ''
-firstNumber = ''
-secondNumber = ''
+import sys
+from pprint import pprint
+pprint(sys.argv)
+'''
+stringParameter = sys.argv[1]
+firstNumber = sys.argv[2]
+secondNumber = sys.argv[3]
 thisSum = ''
 
 print("this is a test of the python script build")
@@ -12,3 +15,4 @@ print("The second number is " + secondNumber)
 thisSum = firstNumber + secondNumber
 print("the sum of " + firstNumber + "and " + secondNumber + " is " + thisSum)
 print("That is all, Happy New Year!")
+'''
