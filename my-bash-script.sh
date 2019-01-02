c70517563e260b681f64be4883b7bc2ec2d52724
Charles Harvey <soops@ucla.edu>
2019-01-02 09:49:22 -0800

    initial commit of script

---
+++ my-bash-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running simple addition with parameters"
+echo $stringParameter
+echo "first number is "; echo $firstNumber
+echo "second number is "; echo $secondNumber
+echo "the sum of $firstNumber and $secondNumber is"
+$sum = $firstNumber + $secondNumber
+echo $sum
+echo "that is all, have a nice day!" 
