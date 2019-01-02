ce568a5562da8e9ede0e2375314f024eeaa1a3ca
Charles Harvey <soops@ucla.edu>
2019-01-02 10:55:20 -0800

    testing python script build

---
+++ myPythonScript.py
@@ -0,0 +1,9 @@
+/usr/bin/python
+
+print("this is a test of the python script build")
+print($stringParameter)
+print("The first number is " . $firstNumber)
+print("The second number is " . $secondNumber)
+$sum = $firstNumber + $secondNumber
+print("the sum of " . $firstNumber . "and " . $secondNumber . " is ". $sum)
+print"That is all, Happy New Year!")
