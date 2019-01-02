067dc5e98a6dbd31d5063fee44f9b55bc81d95d8
Charles Harvey <soops@ucla.edu>
2019-01-02 11:03:23 -0800

    seriously, must python mas!

--- myPythonScript.py
+++ myPythonScript.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
+stringParameter = ''
+firstNumber = ''
+secondNumber = ''
+thisSum = ''
+
 print("this is a test of the python script build")
 print(stringParameter)
-print("The first number is " . firstNumber)
-print("The second number is " . secondNumber)
+print("The first number is " + firstNumber)
+print("The second number is " + secondNumber)
 thisSum = firstNumber + secondNumber
-print("the sum of " . firstNumber . "and " . secondNumber . " is ". thisSum)
-print"That is all, Happy New Year!")
+print("the sum of " + firstNumber + "and " + secondNumber + " is " + thisSum)
+print("That is all, Happy New Year!")
