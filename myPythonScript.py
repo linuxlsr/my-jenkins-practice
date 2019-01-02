cce8d1e13b3c6e8d169850a372de5cab647fd629
Charles Harvey <soops@ucla.edu>
2019-01-02 11:00:23 -0800

    completely forgot how to python

--- myPythonScript.py
+++ myPythonScript.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 print("this is a test of the python script build")
-print($stringParameter)
-print("The first number is " . $firstNumber)
-print("The second number is " . $secondNumber)
-$sum = $firstNumber + $secondNumber
-print("the sum of " . $firstNumber . "and " . $secondNumber . " is ". $sum)
+print(stringParameter)
+print("The first number is " . firstNumber)
+print("The second number is " . secondNumber)
+thisSum = firstNumber + secondNumber
+print("the sum of " . firstNumber . "and " . secondNumber . " is ". thisSum)
 print"That is all, Happy New Year!")
